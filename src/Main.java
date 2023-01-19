ecb5ac43a160dbe3abb69853d3ec1e60129919ca
Vally <murkhonovbatoha@gmail.com>
2023-01-20 05:08:39 +0800

    Тело

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1() {
+        System.out.println((char) 27 + "[34m----------Задача 1----------" + (char)27 + "[0m");
+    }
+
+    public static void task2() {
+        System.out.println((char) 27 + "[34m----------Задача 2----------" + (char)27 + "[0m");
+    }
+
+    public static void task3() {
+        System.out.println((char) 27 + "[34m----------Задача 3----------" + (char)27 + "[0m");
+    }
+
+    public static void task4() {
+        System.out.println((char) 27 + "[34m----------Задача 4----------" + (char)27 + "[0m");
+    }
+
+    public static void task5() {
+        System.out.println((char) 27 + "[34m----------Задача 5----------" + (char)27 + "[0m");
+    }
+
+    public static void task6() {
+        System.out.println((char) 27 + "[34m----------Задача 6----------" + (char)27 + "[0m");
+    }
+
+    public static void task7() {
+        System.out.println((char) 27 + "[34m----------Задача 7----------" + (char)27 + "[0m");
+    }
+
+    public static void task8() {
+        System.out.println((char) 27 + "[34m----------Задача 8----------" + (char)27 + "[0m");
+    }
+}
